64a900422824a5f412373f8f6ff67c8069b28a6e
Saffron Bannister <u6062525@anu.edu.au>
2022-03-26 17:20:40 +1100

    add output mode lol

--- a1/tilt_sensor/tilt_sensor.ino
+++ a1/tilt_sensor/tilt_sensor.ino
@@ -8,20 +8,21 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(tiltSwitch, INPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int switchResult = digitalRead(tiltSwitch);
   Serial.println(switchResult);
-  if (switchResult = HIGH) {
+  if (switchResult = LOW) {
     bwooop();
   }
 }
 
 // make a 'bwooop' sound out of the buzzer
 void bwooop() {
-  for (int i = 0; i < 180; i++) {
+  for (int i = 200; i < 400; i++) {
     tone(buzzer, i, 5);
     delay(5);
   }
