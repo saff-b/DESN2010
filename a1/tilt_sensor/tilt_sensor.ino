08094f73d5623b5db4993cfba215916773abdae6
Saffron Bannister <u6062525@anu.edu.au>
2022-03-26 18:05:31 +1100

    doesn't work but very interesting

--- a1/tilt_sensor/tilt_sensor.ino
+++ a1/tilt_sensor/tilt_sensor.ino
@@ -1,30 +1,37 @@
 // tilt sensor: digital input, when the circuit is wiggled around the plate disconnects
 // note: requires a 240 resistor between the sensor and ground
 
-int tiltSwitch = 9;
+int tiltSwitch = 3;
 int buzzer = 7;
+
+bool bwoop = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(tiltSwitch, INPUT);
   pinMode(buzzer, OUTPUT);
+  // interrupts https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
+  attachInterrupt(digitalPinToInterrupt(tiltSwitch), makeBwoop, FALLING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int switchResult = digitalRead(tiltSwitch);
-  Serial.println(switchResult);
-  if (switchResult = LOW) {
-    bwooop();
-  }
+  //if (bwoop) {
+
+  noInterrupts();
+    //Serial.print("bwooptime!");
+    for (int i = 440; i < 600; i++) {
+      tone(buzzer, i, 50);
+      delay(5);
+    }
+    noTone(buzzer);    
+    //bwoop = false;
+  interrupts();
+  //};
 }
 
-// make a 'bwooop' sound out of the buzzer
-void bwooop() {
-  for (int i = 200; i < 400; i++) {
-    tone(buzzer, i, 5);
-    delay(5);
-  }
-    noTone(buzzer);
+void makeBwoop() {
+  bwoop = true;
+  Serial.println("bwoop");
 }
