1ac7d1fdaf388ab6614e4ae656d4d178912ec9f6
Saffron Bannister <u6062525@anu.edu.au>
2022-04-25 18:47:54 +1000

    motion sensor v1

---
+++ week-7/motion_sensor/motion_sensor.ino
@@ -0,0 +1,32 @@
+/* motion sensor */
+
+int sensorPin = 9;
+int ledPin = 13;
+
+int motion = HIGH; // low = no motion ; high = motion
+
+void setup() {
+  // put your setup code here, to run once:
+  /* Pin setup */
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /* Take a read from the sensor */
+  int sensorData = digitalRead(sensorPin);
+  if (sensorData = HIGH) {
+    if (motion = LOW) {
+      motion = HIGH;
+    }
+    digitalWrite(ledPin, HIGH);
+  } else {
+    if (motion = HIGH) {
+      motion = LOW;
+    }
+    digitalWrite(ledPin, LOW);
+  }
+  Serial.println(sensorData);
+}
